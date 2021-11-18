24fe11c9a5d901f19236cfbf8bea19aff8876dbb
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-11-19 00:52:30 +0300

    fix: В окне редактирования профиля, полям ввода добавлены placeholder

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -42,11 +42,13 @@ function EditProfilePopup(props) {
                    onSubmit={handleSubmit}>
         <input id="name-user-input" className="popup__input popup__input_type_name"
                type="text" name="name" required minLength="2" maxLength="40"
-               value={name || ' '} onChange={handleChangeName} />
+               placeholder="Имя пользователя"
+               value={name || ''} onChange={handleChangeName} />
         <span className="popup__input-error name-user-input-error"></span>
         <input id="description-input" className="popup__input popup__input_type_description"
                type="text" name="description" required minLength="2" maxLength="200"
-               value={description || ' '} onChange={handleChangeDescription} />
+               placeholder="Информация о пользователе"
+               value={description || ''} onChange={handleChangeDescription} />
         <span className="popup__input-error description-input-error"></span>
         <button className="popup__button" type="submit">
           {props.isLoading ? 'Сохранить...' : 'Сохранить'}
